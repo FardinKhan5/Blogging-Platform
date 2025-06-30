12bcc130302ad125e143a071e14196b1d1be5aad
Fardin Khan <153431358+FardinKhan5@users.noreply.github.com>
2025-06-30 16:47:41 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app=express()
 const port=process.env.PORT || 3000
 const mongoUri=process.env.MONGO_URI
 app.use(cors({
-    origin:["*"],
+    origin:"*",
     credentials:true
 }))
 app.use(cookieParser())
